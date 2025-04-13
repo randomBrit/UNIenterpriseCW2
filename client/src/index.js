01932495847eef175719e8b50394fa3756a6665f
randomBrit <connerliamstewart@gmail.com>
2025-04-14 00:05:08 +0100

    honestly no idea chatgpt said this would work, something eslint related

--- client/src/index.js
+++ client/src/index.js
@@ -5,5 +5,9 @@ import App from './App';
 
 console.log("find###########################################################################")
 console.log('Root element:', document.getElementById('root'));
-const root = ReactDOM.createRoot(rootElement);
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
