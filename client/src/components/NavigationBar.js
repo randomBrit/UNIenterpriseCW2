12200a76dd0cb5680c8d5be9885a5654a6366a55
randomBrit <connerliamstewart@gmail.com>
2025-04-15 01:29:56 +0100

    log in button

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import LoginButton from './LoginButton';
 
 function NavigationBar() {
   return (
@@ -9,13 +10,14 @@ function NavigationBar() {
         <Navbar.Brand as={Link} to="/">Microfiction Library</Navbar.Brand>
         <Navbar.Toggle aria-controls="main-navbar" />
         <Navbar.Collapse id="main-navbar">
-          <Nav className="ms-auto">
+        <Nav className="me-auto">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
             <Nav.Link as={Link} to="/submit">Submit</Nav.Link>
             <Nav.Link as={Link} to="/stories">Browse</Nav.Link>
-            <Nav.Link as={Link} to="/login">Login</Nav.Link>
-            {/* You could also add conditional rendering here for logged in users */}
           </Nav>
+          <div className="d-flex align-items-center">
+            <LoginButton />
+          </div>
         </Navbar.Collapse>
       </Container>
     </Navbar>
