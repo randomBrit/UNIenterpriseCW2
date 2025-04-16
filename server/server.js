7d6e704d808c42c35f772ffb43c8e7ddc2b0e385
randomBrit <connerliamstewart@gmail.com>
2025-04-16 22:47:19 +0100

    basicer serving

--- server/server.js
+++ server/server.js
@@ -11,11 +11,11 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connect
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+//mongoose.connect(process.env.MONGO_URI, {
+//  useNewUrlParser: true,
+//  useUnifiedTopology: true,
+//}).then(() => console.log("Connected to MongoDB"))
+//  .catch((err) => console.error("MongoDB connection error:", err));
 
 // Routes
 app.use('/api/stories', require('./routes/stories'));
