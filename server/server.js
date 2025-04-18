82632e2c436aa10b8989ed4ddfb5bd3c48ab6dae
randomBrit <connerliamstewart@gmail.com>
2025-04-18 19:39:36 +0100

    proper api route for story submission

--- server/server.js
+++ server/server.js
@@ -6,19 +6,15 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// MongoDB connect
-//mongoose.connect(process.env.MONGO_URI, {
-//  useNewUrlParser: true,
-//  useUnifiedTopology: true,
-//}).then(() => console.log("Connected to MongoDB"))
-//  .catch((err) => console.error("MongoDB connection error:", err));
 
 // Routes
-app.use('/api/stories', require('./routes/stories'));
+import storyRoutes from './routes/storyRoutes.js';
+app.use('/api/stories', storyRoutes);
 
 app.get('/', (req, res) => res.send('API is running'));
 
