6a94e5f7530bd3083c489a9422dfc23750315274
randomBrit <connerliamstewart@gmail.com>
2025-04-20 23:01:55 +0100

    firebase DB v2 also file contents v13

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -1,4 +1,3 @@
-// server/firebaseAdmin.js
 import { initializeApp, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import serviceAccount from './serviceAccountKey.json' assert { type: 'json' };
