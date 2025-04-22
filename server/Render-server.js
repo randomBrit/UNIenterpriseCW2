aef173eecd7d992b2752e4696bbc5a232c1a0c0c
randomBrit <connerliamstewart@gmail.com>
2025-04-22 02:33:55 +0100

    find the issue

--- server/Render-server.js
+++ server/Render-server.js
@@ -21,6 +21,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//
+app.use((req, res, next) => {
+  console.log(`🕵️ Incoming request: ${req.method} ${req.url}`);
+  next();
+});
+
 // Routes
 import storyRoutes from './routes/storyRoutes.js';
 app.use('/api/stories', storyRoutes);
