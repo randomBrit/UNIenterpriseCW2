872686a9420f3647459e90b33ca8c59b34054e99
randomBrit <connerliamstewart@gmail.com>
2025-04-22 02:52:39 +0100

    safer path

--- server/Render-server.js
+++ server/Render-server.js
@@ -37,7 +37,7 @@ app.get('/', (req, res) => res.send('API is running'));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
 }
